34b3b5f016044dadb0078db90a2e71f7c7bc207a
BR7T <buratinivictor@gmail.com>
2026-06-25 19:36:30 -0300

    .

--- api/main.go
+++ api/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/BR7T/Blog/endpoints"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+      log.Println("✅ Aplicação iniciada")
+    log.Println("Porta:", os.Getenv("PORT"))
+    s , _ :=os.Getwd()
+    log.Println("Diretório atual:", s)
   err := godotenv.Load()
   if err != nil{
 	  return
